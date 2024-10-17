4d8c00e06a1f5d09aff9f023210eca6956feab8b
Jakkapan Attala <Jakkapan.attala@gmail.com>
2024-10-17 23:42:49 +0700

    Update UI

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
 import {SidebarComponent} from './components/sidebar/sidebar.component';
+import {HeaderComponent} from './components/header/header.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
     RouterOutlet,
-    SidebarComponent
+    SidebarComponent,
+    HeaderComponent
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
