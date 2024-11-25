5edc311b53cc3df47013b7c1b98af84209a969b5
Jakkapan-a <jakkapan.attala@gmail.com>
2024-11-26 00:18:22 +0700

    add pagination

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, CUSTOM_ELEMENTS_SCHEMA, OnInit} from '@angular/core';
 import {TranslatePipe} from '@ngx-translate/core';
 import {BreadcrumbsComponent} from '../../shared/breadcrumbs/breadcrumbs.component';
 import {NgClass} from '@angular/common';
 import {StatComponent} from '../../widget/stat/stat.component';
+import {NgbPagination} from '@ng-bootstrap/ng-bootstrap';
 //
 const projectstatData = [{
   title: 'การใช้งานล่าสุด 30 วัน',
@@ -37,14 +38,23 @@ const projectstatData = [{
     BreadcrumbsComponent,
     NgClass,
     StatComponent,
+    NgbPagination,
   ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class DashboardComponent implements OnInit {
   breadCrumbItems!: Array<{}>;
   statData!: any;
 
+  content!: Array<{}>;
+  order: string = 'asc';
+  sort: string = 'id';
+  page: number = 1;
+  limit: number = 10;
   constructor() {
     this.breadCrumbItems = [
       { label: '', active: true }
@@ -53,5 +63,40 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this.statData = projectstatData;
+
+    this.content = [
+      {
+        title: 'การใช้งานล่าสุด 30 วัน',
+        value: 825,
+        image: "assets/images/dashboard/1.png",
+        persantage: '5.02',
+        profit: 'down',
+      },
+      {
+        title: 'ยอดใช้งานแล้ว',
+        value: 7522,
+        image: "assets/images/dashboard/2.png",
+        persantage: '3.58',
+        profit: 'up',
+      },
+      {
+        title: 'ยอดจองรอใช้งาน',
+        value: 168.40,
+        image: "assets/images/dashboard/3.png",
+        persantage: '10.35 ',
+        profit: 'down',
+      },
+      {
+        title: 'ยอดจองรอใช้งาน',
+        value: 168.40,
+        image: "assets/images/dashboard/4.png",
+        persantage: '10.35 ',
+        profit: 'down',
+      }
+    ];
+  }
+
+  changePage(){
+
   }
 }
