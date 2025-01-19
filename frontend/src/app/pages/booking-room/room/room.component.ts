aa5fa45c291d2c5fcd5f6468350cf35cd655fc64
Jakkapan-a <jakkapan.attala@gmail.com>
2025-01-19 09:50:02 +0700

    Update create book info

--- frontend/src/app/pages/booking-room/room/room.component.ts
+++ frontend/src/app/pages/booking-room/room/room.component.ts
@@ -1,22 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, CUSTOM_ELEMENTS_SCHEMA, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {BreadcrumbsComponent} from '../../../shared/breadcrumbs/breadcrumbs.component';
 
 @Component({
   selector: 'app-room',
   standalone: true,
-  imports: [],
+  imports: [
+    BreadcrumbsComponent
+  ],
   templateUrl: './room.component.html',
-  styleUrl: './room.component.scss'
+  styleUrl: './room.component.scss',
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class RoomComponent implements OnInit {
   roomId: number = 0;
+  breadCrumbItems!: Array<{}>;
   constructor(private route: ActivatedRoute) {
+    this.breadCrumbItems = [
+      { label: 'Dashboard' },
+      { label: 'Booking Room'},
+      { label: 'Room', active: true }
+    ];
 
   }
 
   ngOnInit() {
     this.roomId = Number(this.route.snapshot.paramMap.get('id'));
 
-    
+
   }
 }
