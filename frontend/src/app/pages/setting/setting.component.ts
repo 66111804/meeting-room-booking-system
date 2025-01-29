f217f2db18cb7876d1825ce7007fe41f2fd437ad
Jakkapan-a <jakkapan.attala@gmail.com>
2025-01-30 00:28:57 +0700

    user from

--- frontend/src/app/pages/setting/setting.component.ts
+++ frontend/src/app/pages/setting/setting.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {BreadcrumbsComponent} from "../../shared/breadcrumbs/breadcrumbs.component";
 
 @Component({
   selector: 'app-setting',
   standalone: true,
-  imports: [],
+    imports: [
+        BreadcrumbsComponent
+    ],
   templateUrl: './setting.component.html',
   styleUrl: './setting.component.scss'
 })
 export class SettingComponent {
+
+  breadCrumbItems!: Array<{}>;
+
+  constructor() {
+    this.breadCrumbItems = [{ label: 'Home', path: '/' }, { label: 'Setting', active: true }];
+  }
 
 }
