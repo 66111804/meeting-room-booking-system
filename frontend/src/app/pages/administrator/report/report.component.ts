11a80ca58d184af29f19d2811d2f6edb5549c909
jakkapan-a <jakkapan.attala@gmail.com>
2025-04-04 22:45:38 +0700

    Update

--- frontend/src/app/pages/administrator/report/report.component.ts
+++ frontend/src/app/pages/administrator/report/report.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
+import {BreadcrumbsComponent} from '../../../shared/breadcrumbs/breadcrumbs.component';
+import {FeatherModule} from 'angular-feather';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-report',
   standalone: true,
-  imports: [],
+  imports: [
+    BreadcrumbsComponent,
+    FeatherModule,
+    RouterLink
+  ],
   templateUrl: './report.component.html',
   styleUrl: './report.component.scss'
 })
 export class ReportComponent {
-
+  breadCrumbItems!: Array<{}>;
   constructor() {
     document.getElementById('elmLoader')?.classList.remove('d-none');
+    this.breadCrumbItems = [
+      {label: 'Administrator'},
+      {label: 'Report', active: true}
+    ];
   }
 
   ngOnInit() {
