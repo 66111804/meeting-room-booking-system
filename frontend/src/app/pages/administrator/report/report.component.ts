d4cc2b3b8c58a00739ee10e24d09b04311f23fb2
jakkapan-a <jakkapan.attala@gmail.com>
2025-04-04 22:53:49 +0700

    Add date picker

--- frontend/src/app/pages/administrator/report/report.component.ts
+++ frontend/src/app/pages/administrator/report/report.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {BreadcrumbsComponent} from '../../../shared/breadcrumbs/breadcrumbs.component';
 import {FeatherModule} from 'angular-feather';
 import {RouterLink} from '@angular/router';
+import {MatFormField} from '@angular/material/form-field';
+import {
+  MatDatepickerActions, MatDatepickerApply, MatDatepickerCancel,
+  MatDatepickerToggle,
+  MatDateRangeInput,
+  MatDateRangePicker
+} from '@angular/material/datepicker';
+import {MatButton} from '@angular/material/button';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-report',
@@ -9,10 +18,22 @@ import {RouterLink} from '@angular/router';
   imports: [
     BreadcrumbsComponent,
     FeatherModule,
-    RouterLink
+    RouterLink,
+    MatFormField,
+    MatDateRangeInput,
+    MatDatepickerToggle,
+    MatDateRangePicker,
+    MatDatepickerActions,
+    MatButton,
+    MatDatepickerCancel,
+    MatDatepickerApply
   ],
+  providers:[provideNativeDateAdapter()],
   templateUrl: './report.component.html',
-  styleUrl: './report.component.scss'
+  styleUrl: './report.component.scss',
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class ReportComponent {
   breadCrumbItems!: Array<{}>;
@@ -23,6 +44,7 @@ export class ReportComponent {
       {label: 'Report', active: true}
     ];
   }
+
 
   ngOnInit() {
   }
